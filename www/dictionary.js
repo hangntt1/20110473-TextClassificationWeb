63cd4b4fff9743108b91005dcec85a9c09b3761d
Glitch (hello-express) <none>
2023-04-24 16:14:40 +0000

    🐀🏄 Checkpoint ./server.js:57422018/2311 ./www/index.html:57422018/3006 ./www/style.css:57422018/2849 ./www/script.js:57422018/14175 ./.glitch-assets:57422018/731 ./www/dictionary.js:57422018/287 ./package.json:57422018/1816

---
+++ www/dictionary.js
@@ -0,0 +1,16 @@
+// Special cases. Export as constants.
+export const PAD =  0;
+export const START = 1;
+export const UNKNOWN = 2;
+
+// Export a lookup object.
+export const LOOKUP = {
+  "i": 3,
+  "check": 4,
+  "video": 5,
+  "song": 6,
+  "com": 7,
+  "please": 8,
+  "like": 9
+  // and all the other words...
+}
